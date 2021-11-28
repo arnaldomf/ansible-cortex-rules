747b0f755e4e9b7ac3355740f09868ce6ba95999
Arnaldo Mendonca <arnaldo.mendonca@luizalabs.com>
2021-11-28 18:54:36 -0300

    refactor: log setup to ansible package refactor: move configuration apply decision to ansible package

--- ansible/ansible.go
+++ ansible/ansible.go
@@ -19,7 +19,21 @@ const (
 	GroupStateNeedToChange = "NEED_CHANGE"
 )
 
-func (m *ModuleConfiguration) CompareState(logger *log.Logger) AnsibleCortexState {
+func (m *ModuleConfiguration) Run(logger *log.Logger) error {
+	state, err := m.CompareState(logger)
+	if state.GroupFailed() {
+		return err
+	}
+	if state.GroupNeedToChange() || state.GroupNotFound() {
+		err = m.ApplyChange(&state)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (m *ModuleConfiguration) CompareState(logger *log.Logger) (AnsibleCortexState, error) {
 	ansibleState := AnsibleCortexState{logger: logger}
 	ruleGroupFromServer, err := m.cortexRuler.GetRuleGroup(m.Namespace, m.RuleGroup.Name)
 	ansibleState.RulerGroup = ruleGroupFromServer
@@ -38,10 +52,10 @@ func (m *ModuleConfiguration) CompareState(logger *log.Logger) AnsibleCortexStat
 		ansibleState.logger.Printf("CompareRuleGroups: %s", ansibleState.state)
 		m.response.Message = err.Error()
 		m.response.Unreachable = true
-		return ansibleState
+		return ansibleState, err
 	}
 
-	return ansibleState
+	return ansibleState, nil
 }
 
 func (m *ModuleConfiguration) ApplyChange(state *AnsibleCortexState) error {
--- ansible/config.go
+++ ansible/config.go
@@ -7,6 +7,8 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
+	"log"
+	"os"
 )
 
 type Configuration struct {
@@ -24,9 +26,11 @@ type ModuleConfiguration struct {
 	models.RuleGroup
 	RootURL       string `json:"root_url"`
 	Namespace     string `json:"namespace"`
+	LogFilePath   string `json:"log_file_path,omitempty"`
 	cortexRuler   *services.CortexRuler
 	configuration *Configuration
 	response      *AnsibleResponse
+	logger        *log.Logger
 }
 
 type AnsibleResponse struct {
@@ -34,6 +38,28 @@ type AnsibleResponse struct {
 	Changed     bool   `json:"changed"`
 	Failed      bool   `json:"failed"`
 	Unreachable bool   `json:"unreachable"`
+}
+
+func (m *ModuleConfiguration) logSetup() *log.Logger {
+	if m.logger != nil {
+		return m.logger
+	}
+	var writer io.Writer
+	if len(m.LogFilePath) == 0 {
+		writer = io.Discard
+	} else {
+		f, err := os.OpenFile(m.LogFilePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			panic(err)
+		}
+		writer = f
+	}
+	return log.New(writer, "", log.LstdFlags)
+}
+
+func (m *ModuleConfiguration) Logger() *log.Logger {
+	m.logger = m.logSetup()
+	return m.logger
 }
 
 func (m *ModuleConfiguration) ResponseMessage() string {
@@ -48,9 +74,9 @@ func (m *ModuleConfiguration) RenderResponse() string {
 	return string(data)
 }
 
-func ModuleSetup(ctx context.Context, reader io.Reader) (*ModuleConfiguration, error) {
+func ModuleSetup(ctx context.Context, ansibleInputFile io.Reader) (*ModuleConfiguration, error) {
 	moduleConfiguration := &ModuleConfiguration{response: new(AnsibleResponse)}
-	input, err := ioutil.ReadAll(reader)
+	input, err := ioutil.ReadAll(ansibleInputFile)
 	if err != nil {
 		return moduleConfiguration, err
 	}
